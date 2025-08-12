0e16a06b86c8afd36a9f950216496b051d084367
Valeriy <RomanovskiyVD@it.mos.ru>
2025-08-12 18:26:38 +0300

    Обновил файл с тестами

--- src/test/java/com/github/DemoQATest2025.java
+++ src/test/java/com/github/DemoQATest2025.java
@@ -1,12 +1,37 @@
 package com.github;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class DemoQATest2025 {
 
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all tests!\n");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After all tests!\n");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before Test!\n");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("After test!\n");
+    }
+
     @Test
     void firstTest() {
-        System.out.println("I love you QA.GURU");
+        System.out.println("I love you QA.GURU\n");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("I love you QA.GURU-secondTest\n");
     }
 
 }
