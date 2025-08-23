273435d7cd2c0c03a0ca27d417d010bab2862f1f
Valeriy <RomanovskiyVD@it.mos.ru>
2025-08-23 21:03:03 +0300

    Добавил шаги для теста из ДЗ

--- src/test/java/com/github/tests/PracticeForm.java
+++ src/test/java/com/github/tests/PracticeForm.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,10 +21,12 @@ public class PracticeForm {
         $("#lastName").setValue("Romanovskiy");
         $("#userEmail").setValue("trasikVdatsunes@yandex.ru");
         $(".custom-control-label").click();
-        $("#userNumber").setValue("333444555");
+        $("#userNumber").setValue("3123312333");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("1995");
         $(".react-datepicker__month-select").selectOptionByValue("0");
-        $(".react-datepicker__day react-datepicker__day--031").click();
+        $(byText("31")).click();
+        $("#submit").click();
+        //$(".react-datepicker__day react-datepicker__day--031").click();
     }
 }
