07dfdd42c0438cb8a6412aee1006d4a510fd1d90
Valeriy <RomanovskiyVD@it.mos.ru>
2025-08-23 20:43:49 +0300

    Добавил новый файл с описанием разницы xpath и css + новый тест

---
+++ src/test/java/com/github/tests/CssXpathExamples.java
@@ -0,0 +1,54 @@
+package com.github.tests;
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CssXpathExamples {
+
+    void cssXpathExamples() {
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="email">
+
+        $("[data-testid=email]");
+        $(by("data-testid", "email"));
+
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="email">
+
+        $("#email");
+        $(byId("email"));
+        $(By.id("email"));
+        $("[id=email]");
+        $("[id='email']");
+        $("[id=\"email\"]");
+        $(by("id", "email"));
+        $x("//*[@id='email']");
+        $(byXpath("//*[@id='email']"));
+
+        $("input#email");
+        $("input[id=email]");
+
+
+        // <input type="email" class="inputtext login_form_input_box" name="email">
+
+        $("[name=email]");
+        $(byName("email"));
+
+        // <input type="email" class="inputtext login_form_input_box">
+        $(byClassName("login_form_input_box"));
+        $(".login_form_input_box");
+        $(".inputtext.login_form_input_box");
+        $x("//*[@class='login_form_input_box']");
+
+        // <div type="email" class="inputtext">
+        //      <input class="login_form_input_box">
+        // </div>
+        $(".inputtext .login_form_input_box");
+
+
+        // <div>Hello qa.guru</div>
+        $(byText("Hello qa.guru"));
+        $(withText("lo qa.guru"));
+    }
+}
---
+++ src/test/java/com/github/tests/PracticeForm.java
@@ -0,0 +1,29 @@
+package com.github.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeForm {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize="maximize";
+    }
+
+    @Test
+    void PtForm(){
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Valeriy");
+        $("#lastName").setValue("Romanovskiy");
+        $("#userEmail").setValue("trasikVdatsunes@yandex.ru");
+        $(".custom-control-label").click();
+        $("#userNumber").setValue("333444555");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1995");
+        $(".react-datepicker__month-select").selectOptionByValue("0");
+        $(".react-datepicker__day react-datepicker__day--031").click();
+    }
+}
