9d876802c0ed576a36df7e83312c6893a8d70544
Valeriy <RomanovskiyVD@it.mos.ru>
2025-08-27 22:53:58 +0300

    Обновил тест из ДЗ (проверка таблицы)

--- src/test/java/com/github/tests/PracticeForm.java
+++ src/test/java/com/github/tests/PracticeForm.java
@@ -1,14 +1,16 @@
 package com.github.tests;
 
-import com.codeborne.selenide.Condition;
+//import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 //import io.github.bonigarcia.wdm.WebDriverManager;
 
+//import java.io.File;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
+//import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -16,7 +18,7 @@ public class PracticeForm {
     @BeforeAll
     static void beforeAll(){
         //WebDriverManager.chromedriver().setup();
-        Configuration.browserSize="maximize";
+        Configuration.browserSize="1920x1080";
         Configuration.baseUrl="https://demoqa.com";
     }
 
@@ -39,20 +41,37 @@ public class PracticeForm {
         //$(".react-datepicker__month-select").selectOptionByValue("0");
         $(".react-datepicker__month-select").selectOption("July");
         //$(byText("31")).click();
-        //$(".react-datepicker__day__028:not(.react-datepicker__day--outside-month)").click(); // выберет такое число 28, которое не содержит класс "outside-month"
-        // $$ - массив элементов
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click(); // выберет такое число 28, которое не содержит класс "outside-month"
+        // $$ - массив элементов, возвращает все элементы
         //$$(".react-datepicker__day--028").filter(not(cssClass("react-datepicker__day--outside-month"))).first().click();
-        $x("//div[contains(@aria-label,\"June 28th, 2005\")]").click(); //*-любой элемент, но напишем div
+        //$x("//div[contains(@aria-label,\"June 28th, 2005\")]").click(); //*-любой элемент, но напишем div
+        // [] означают, что будут указывать атрибут и его значения
+        //contains Не работает для css-селекторов, только для xpath
 
         $("#subjectsInput").setValue("Chemistry").pressEnter();
-        $("label[for='hobbies-checkbox-1']").click();
+
+        //$("label[for='hobbies-checkbox-1']").click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
         $("#uploadPicture").uploadFromClasspath("moon.jpg");
+        //$("#uploadPicture").uploadFromClasspath("img/moon.jpg"); если файл в папке
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/moon.jpg"));
+
         $("#currentAddress").setValue("Test");
+
         $("#react-select-3-input").setValue("NCR").pressEnter();
+        //$("#stateCity-wrapper").$(byText("NCR")).click();
+
         $("#react-select-4-input").setValue("Gurgaon").pressEnter();
         $("#submit").scrollTo().click();
 
+        //$(".modal-title").shouldHave(text("Thanks for submitting the form"));
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(byText("Student Name")).shouldHave(text("Valeriy Romanovskiy"));
+
+        $(".table-responsive").shouldHave(text("Valeriy Romanovskiy"), text("testik@ya.ru"),
+                text("Male"), text("3123312333"), text("31 January,1995"), text("Chemistry"), text("Reading"),
+                 text("moon.jpg"), text("Test123"), text("NCR Delhi"));
+
+        $("#closeLargeModal").click();
     }
 }
